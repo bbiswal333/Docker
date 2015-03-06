5c27e4abd7c927b1c1f39ddc668c961aef24c485
Gerald BRAUNWARTH <gerald.braunwarth@sap.com>
2015-03-06 16:09:02 +0100

    added comments

--- Redhat/build/Hana/092/PostBuildHana092-BuildOps.sh
+++ Redhat/build/Hana/092/PostBuildHana092-BuildOps.sh
@@ -8,6 +8,7 @@ REV=092
 
 
 # BuildOps tool requires 2 levels of TAR
+# output  = hana092.tar.tar
 tar -cvf hana$REV.tar.tar  hana$REV.tar
 if [ $? != 0 ]; then
   echo "Failed to TAR 'hana$REV.tar'"
@@ -15,6 +16,7 @@ if [ $? != 0 ]; then
 
 
 # High compression before copying to NEXUS
+# output = hana092.tar.tar.gz
 gzip hana$REV.tar.tar
 if [ $? != 0 ]; then
   echo "Failed to GZIP 'hana$REV.tar.tar'"
