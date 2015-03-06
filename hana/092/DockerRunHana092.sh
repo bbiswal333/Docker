f4aeeb3f050d31cdfd742c3d64a80da607542774
Gerald BRAUNWARTH <gerald.braunwarth@sap.com>
2015-03-06 14:52:51 +0100

    Delete DockerRunHana092.sh

--- hana/092/DockerRunHana092.sh
+++
@@ -1,9 +0,0 @@
-
-###############################################################################
-#
-#  AUTHOR: gerald.braunwarth@sap.com
-#
-###############################################################################
-
-docker run  -it  --name=hana092-00  --privileged  --net=host  -v etc/localtime:/etc/localtime -v /root/docker/build/hana/092/StartHana.sh:/StartHana.sh   dewdftzlidck:5000/hana/hana092  /bin/sh -c /StartHana.sh
-
