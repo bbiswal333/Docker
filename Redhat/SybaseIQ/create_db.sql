e9faeba909179eb5add1814776486c7ba6d20a6e
root <root@geraldvmrh7Vero.dhcp.pgdev.sap.corp>
2015-03-26 17:26:55 +0100

    Add SybaseIQ dockerfile

---
+++ Redhat/SybaseIQ/create_db.sql
@@ -0,0 +1,14 @@
+create database  '/sybase/DB/cms16iq.db'
+transaction log on '/sybase/DB/cms16iq_a.rdo'
+mirror '/sybase/DB/cms16iq_b.rdo'
+iq path '/sybase/DB/cms16iq.iq'
+iq size 1024
+iq reserve 16384
+message path '/sybase/DB/cms16iq.iqmsg'
+temporary path '/sybase/DB/cms16iq.iqtmp'
+temporary size 1024
+temporary reserve 16384
+collation 'UTF8BIN'
+encoding 'UTF-8'
+java on
+jconnect on
