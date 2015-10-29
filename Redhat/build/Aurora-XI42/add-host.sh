537385ae5e93a31f28134cfb611ef5d7ea43c399
Gerald BRAUNWARTH <gerald.braunwarth@sap.com>
2015-10-29 10:02:36 +0100

    Optimized

--- Redhat/build/Aurora-XI42/add-host.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-host1line=`grep \`uname -n\` /etc/hosts`
-cp /etc/hosts /etc/hosts.old
-host1line=`grep \`uname -n\` /etc/hosts`
-sed  "/$host1line/s/$/ sapboxi42/" /etc/hosts.old > /etc/hosts
-
-
