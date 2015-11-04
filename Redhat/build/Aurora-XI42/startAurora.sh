3b74ede82227b8a671fdb2f9a2a3df190cfd8e62
Gerald BRAUNWARTH <gerald.braunwarth@sap.com>
2015-11-04 13:02:36 +0100

    Optimized

--- Redhat/build/Aurora-XI42/startAurora.sh
+++ Redhat/build/Aurora-XI42/startAurora.sh
@@ -10,3 +10,5 @@ su - qaunix -c "
   /usr/sap/XI42/sap_bobj/sqlanywhere_startup.sh
   /usr/sap/XI42/sap_bobj/startservers
   /usr/sap/XI42/sap_bobj/tomcatstartup.sh"
+
+while true; do sleep 300; done
