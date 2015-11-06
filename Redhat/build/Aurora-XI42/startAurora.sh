7079fc48e3f2b950284792b07b5f9c909e031964
Gerald BRAUNWARTH <gerald.braunwarth@sap.com>
2015-11-06 10:27:50 +0100

    4.2 becomes 4.x

--- Redhat/build/Aurora-XI42/startAurora.sh
+++ Redhat/build/Aurora-XI42/startAurora.sh
@@ -7,8 +7,8 @@
 
 su - qaunix -c "
 
-  /usr/sap/XI42/sap_bobj/sqlanywhere_startup.sh
-  /usr/sap/XI42/sap_bobj/startservers
-  /usr/sap/XI42/sap_bobj/tomcatstartup.sh"
+  /usr/sap/XI4x/sap_bobj/sqlanywhere_startup.sh
+  /usr/sap/XI4x/sap_bobj/startservers
+  /usr/sap/XI4x/sap_bobj/tomcatstartup.sh"
 
 while true; do sleep 300; done
