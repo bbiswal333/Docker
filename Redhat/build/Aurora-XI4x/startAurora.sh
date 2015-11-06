7c288d02ce066b3e31f8dca1684bded7c7d95327
Gerald BRAUNWARTH <gerald.braunwarth@sap.com>
2015-11-06 12:47:46 +0100

    Alias also for startservers

--- Redhat/build/Aurora-XI4x/startAurora.sh
+++ Redhat/build/Aurora-XI4x/startAurora.sh
@@ -5,6 +5,14 @@
 ###############################################################################
 
 
+# ALIAS in /etc/hosts
+cp /etc/hosts /etc/hosts.old
+if grep 127.0.0.1 /etc/hosts > /dev/null; then
+  sed "/127.0.0.1/s/localhost/localhost  $(hostname -s)  sapboxi4x  /" /etc/hosts.old > /etc/hosts
+else
+  echo "127.0.0.1  localhost  $(hostname -s)  sapboxi4x" >> /etc/hosts; fi
+
+
 su - qaunix -c "
 
   /usr/sap/XI4x/sap_bobj/sqlanywhere_startup.sh
