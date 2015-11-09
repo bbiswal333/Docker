3d9e8e1fb012e45a414249a801534a4d7169f56e
Gerald BRAUNWARTH <gerald.braunwarth@sap.com>
2015-11-09 11:13:59 +0100

    Source code improvement

--- Redhat/build/Aurora-XI4x/api-build.sh
+++ Redhat/build/Aurora-XI4x/api-build.sh
@@ -1,44 +1,39 @@
+
 # api-build.sh
 # simon.gomez@sap.com
 
-# Script to build the aurora image on your swarm cluster
- 
-#   PARAMETER : SwarmManagerSrv-FQDN
+#   PURPOSE: Script to build the aurora image on your swarm cluster
+#   PARAMETER: SwarmManagerSrv-FQDN
 
 
 #!/bin/bash
 
 if [ $# -ne 1 ]; then
-echo "Usage api-build.sh <SwarmManagerSrv-FQDN>"
-exit 1;
+  echo "Usage api-build.sh <SwarmManagerSrv-FQDN>"
+  exit 1;
 fi
 
 
-
-#	IMAGES ON THE CLUSTER BEFORE THE BUILD
-
+## DEBUG PURPOSE
+#       IMAGES ON THE CLUSTER BEFORE THE BUILD
 echo 'Images on the swarm cluster before the build :'
-curl dewdftv01641.dhcp.pgdev.sap.corp:4000/images/json?all=1
+curl $1:4000/images/json?all=1
+##
 
 
-#	DOWNLOADING DOCKERFILE
-
-curl -k -s  https://github.wdf.sap.corp/raw/Dev-Infra-Levallois/Docker/master/Redhat/build/Aurora-XI4x/Dockerfile > Dockerfile
-
-#	BUILD PROCESS THROUGH THE SWARM API 
-
+#       DOWNLOADING DOCKERFILE
 mkdir aurora
-mv Dockerfile aurora/
-cd aurora/
-tar zcf Dockerfile.tar.gz Dockerfile
-cd ..
+curl -k -s  https://github.wdf.sap.corp/raw/Dev-Infra-Levallois/Docker/master/Redhat/build/Aurora-XI4x/Dockerfile > aurora/Dockerfile
 
+
+#       BUILD PROCESS THROUGH THE SWARM API
+tar zcf aurora/Dockerfile.tar.gz aurora/Dockerfile
 curl -v -X POST -H "Content-Type:application/tar" --data-binary '@aurora/Dockerfile.tar.gz' $1:4000/build?t=aurora-image\&forcerm=1
-rm aurora/Dockerfile aurora/Dockerfile.tar.gz
-rmdir aurora
+rmdir -rf aurora
 
 
-#	RESULT: IMAGES ON THE CLUSTER AFTER THE BUILD
+## DEBUG PURPOSE
+#       RESULT: IMAGES ON THE CLUSTER AFTER THE BUILD
 echo 'Images on the swarm cluster after the build :'
-curl dewdftv01641.dhcp.pgdev.sap.corp:4000/images/json?all=1
-
+curl $1:4000/images/json?all=1
+##
