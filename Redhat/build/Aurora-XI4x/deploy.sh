accfc8a2ee3ae00aa63de8a10f79c98f3e398949
Simon GOMEZ <simon.gomez@sap.com>
2016-01-14 14:53:58 +0100

    Update and rename verifAndDeploy.sh to deploy.sh

--- Redhat/build/Aurora-XI4x/deploy.sh
+++ Redhat/build/Aurora-XI4x/deploy.sh
@@ -1,13 +1,4 @@
-#You need to have a "lastrepo.txt" file on your workspace
 
-set -x
-version=`curl -k https://github.wdf.sap.corp/raw/AuroraXmake/aurora4xInstall/master/version.txt`
-ls /net/derotvi0127.pgdev.sap.corp/derotvi0127e_bobj/q_unix/Imagesdck/repositories/aurora > newrepo.txt
-fgrep -vf lastrepo.txt newrepo.txt > temp.txt
-cat temp.txt
-cat newrepo.txt > lastrepo.txt
-grep aurora42_${version} temp.txt
-if [ $? -eq 0 ];  then 
 	
 			if [ ! -f swarm-request.ini ]; then
 		    	curl -k https://github.wdf.sap.corp/raw/Dev-Infra-Levallois/Docker/master/swarm/automation/swarm-request.ini > swarm-request.ini; fi
