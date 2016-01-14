2a83f353f5b9eab9a5743f50173846533ab41338
Simon GOMEZ <simon.gomez@sap.com>
2016-01-14 15:05:07 +0100

    Update deploy.sh

--- Redhat/build/Aurora-XI4x/deploy.sh
+++ Redhat/build/Aurora-XI4x/deploy.sh
@@ -1,6 +1,6 @@
 
 	
-			if [ ! -f swarm-request.ini ]; then
+		if [ ! -f swarm-request.ini ]; then
 		    	curl -k https://github.wdf.sap.corp/raw/Dev-Infra-Levallois/Docker/master/swarm/automation/swarm-request.ini > swarm-request.ini; fi
 		curl -k https://github.wdf.sap.corp/raw/Dev-Infra-Levallois/Docker/master/swarm/automation/swarmHA-run.sh > swarmHA-run.sh
 		curl -k https://github.wdf.sap.corp/raw/Dev-Infra-Levallois/Docker/master/swarm/automation/swarm-listnodes.sh > swarm-listnodes.sh
@@ -10,8 +10,10 @@
 		chmod +r swarm-request.ini
 		
 		image="dockerdevregistry:5000/aurora/aurora42_${version}-snapshot"
-		/bin/bash ${WORKSPACE}/swarmHA-run.sh 2   "$image"
-		/bin/bash ${WORKSPACE}/swarm-listnodes.sh "$image"
-		cat nodesList.txt; fi 
+
+		./swarmHA-run.sh 2   "$image"
+		./swarm-listnodes.sh "$image"
+
+		cat nodesList.txt
 
 
