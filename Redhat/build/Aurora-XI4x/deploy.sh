f65f0ec2c2a52e3f065700c307d2b8efa878c5ef
Gerald BRAUNWARTH <gerald.braunwarth@sap.com>
2016-01-18 14:58:22 +0100

    Added silent mode to curl and output echo commands

--- Redhat/build/Aurora-XI4x/deploy.sh
+++ Redhat/build/Aurora-XI4x/deploy.sh
@@ -1,19 +1,24 @@
 
-		version=`curl -k https://github.wdf.sap.corp/raw/AuroraXmake/aurora4xInstall/master/version.txt`
-		if [ ! -f swarm-request.ini ]; then
-		    	curl -k https://github.wdf.sap.corp/raw/Dev-Infra-Levallois/Docker/master/swarm/automation/swarm-request.ini > swarm-request.ini; fi
-		curl -k https://github.wdf.sap.corp/raw/Dev-Infra-Levallois/Docker/master/swarm/automation/swarmHA-run.sh > swarmHA-run.sh
-		curl -k https://github.wdf.sap.corp/raw/Dev-Infra-Levallois/Docker/master/swarm/automation/swarm-listnodes.sh > swarm-listnodes.sh
-		
-		chmod +x swarmHA-run.sh
-		chmod +x swarm-listnodes.sh
-		chmod +r swarm-request.ini
-		
-		image="dockerdevregistry:5000/aurora/aurora42_${version}-snapshot"
+version=`curl -k -s https://github.wdf.sap.corp/raw/AuroraXmake/aurora4xInstall/master/version.txt`
 
-		./swarmHA-run.sh 2   "$image"
-		./swarm-listnodes.sh "$image"
+echo
+if [ ! -f swarm-request.ini ]; then
+  echo Getting script swarm-request.ini
+  curl -k -s https://github.wdf.sap.corp/raw/Dev-Infra-Levallois/Docker/master/swarm/automation/swarm-request.ini > swarm-request.ini; fi
 
-		cat nodesList.txt
+echo Getting script swarmHA-run.sh
+curl -k -s https://github.wdf.sap.corp/raw/Dev-Infra-Levallois/Docker/master/swarm/automation/swarmHA-run.sh > swarmHA-run.sh
 
+echo Getting script swarm-listnodes.sh
+curl -k -s https://github.wdf.sap.corp/raw/Dev-Infra-Levallois/Docker/master/swarm/automation/swarm-listnodes.sh > swarm-listnodes.sh
 
+chmod +x swarmHA-run.sh
+chmod +x swarm-listnodes.sh
+chmod +r swarm-request.ini
+
+image="dockerdevregistry:5000/aurora/aurora42_${version}-snapshot"
+
+./swarmHA-run.sh 2   "$image"
+./swarm-listnodes.sh "$image"
+
+cat nodesList.txt
