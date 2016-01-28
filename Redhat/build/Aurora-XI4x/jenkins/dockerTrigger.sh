c0b07a670cbb28390cb9a99f6cd223ac4eb5a231
Gerald BRAUNWARTH <gerald.braunwarth@sap.com>
2016-01-28 15:06:45 +0100

    Initial upload

---
+++ Redhat/build/Aurora-XI4x/jenkins/dockerTrigger.sh
@@ -0,0 +1,20 @@
+# The file lastrepo.txt must exists, it contains the previous repositories list before changes
+
+#set -x
+
+if [ ! -f lastrepo.txt ]; then
+  echo "Missing file 'lastrepo.txt' that contains the previous inventory to be compared"
+  exit 1; fi
+
+version=`curl -s -k https://github.wdf.sap.corp/raw/AuroraXmake/aurora4xInstall/master/version.txt`
+dockerrepo="/net/derotvi0127.pgdev.sap.corp/derotvi0127e_bobj/q_unix/Imagesdck/repositories/aurora"
+
+ls $dockerrepo>newrepo.txt
+
+fgrep -vf lastrepo.txt newrepo.txt | grep aurora42_${version}
+status=$?
+
+rm -f lastrepo.txt
+mv  newrepo.txt lastrepo.txt
+
+exit $status
