a397dec83e822d1b225ae5a2acf2e24294024eea
Gerald BRAUNWARTH <gerald.braunwarth@sap.com>
2016-02-05 15:09:40 +0100

    update

--- swarm/automation/jenkins/write-request.sh
+++ swarm/automation/jenkins/write-request.sh
@@ -8,16 +8,18 @@ if [ -f $file ]; then
 # ENDDEBUG
 
 if [ ! -f $file ]; then
+  printf "\n# DISCOVERY\n# -----\n"       >> $file
   printf "zookeepers=\"$zookeepers\"\n"   >> $file
-  printf "managers\"$managers\"\n"       >> $file
+  printf "\n# MANAGERS\n# -----\n"        >> $file
+  printf "managers=\"$managers\"\n"       >> $file
   printf "\n# NODES\n# -----\n"           >> $file
-  printf "nodes\"$nodes\"\n"             >> $file
+  printf "nodes=\"$nodes\"\n"             >> $file
   printf "\n# CLUSTER ID\n# ----------\n" >> $file
-  printf "token\"$token\"\n"             >> $file
+  printf "token=\"$token\"\n"             >> $file
   printf "\n# SECURITY\n# ----------\n"   >> $file
-  printf "\"tls$tls\"\n"                 >> $file
-  printf "engineport\"$engineport\"\n"   >> $file
-  printf "managerport\"$managerport\"\n" >> $file; fi
+  printf "tls=\"$tls\"\n"                 >> $file
+  printf "engineport=\"$engineport\"\n"   >> $file
+  printf "managerport=\"$managerport\"\n" >> $file; fi
 
 echo
 cat $file
