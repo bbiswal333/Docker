5631adc6feec978720958a57ca7d2005e42b369d
Gerald BRAUNWARTH <gerald.braunwarth@sap.com>
2016-03-25 16:28:59 +0100

    Launch BUILDOPS smokeTest

--- Redhat/build/Aurora-XI4x/jenkins/deploy.sh
+++ Redhat/build/Aurora-XI4x/jenkins/deploy.sh
@@ -119,17 +119,7 @@ function RetrieveDeployedNodes {
 #--------------------------------------
 function WriteConnnectionFile {
 
-  ## retrieve nodeone IP 
-  #ping=`ping -c 1 $nodeone 2>&1 | grep "("`
-  #if [ ! "${ping}" ]; then
-  #  echo "Failed to retrieve $nodeone IP"
-  #  exit 1; fi
-  #IP=`echo $ping | awk '$3 { print $3 }'`
-  #IP=${IP/(/}
-  #IP=${IP/)/}
-
-
-  ## WRITE connectinfo.ini
+  # Write file connectinfo.ini
   echo
   GetFromGithub $gitResponse $response .
 
@@ -140,25 +130,81 @@ function WriteConnnectionFile {
 
   source $response
 
-  echo BUILD_NUMBER=$version              >  $connectinfo
-  echo BUILD_STREAM=$1                    >> $connectinfo
-  echo ip=$nodeone                        >> $connectinfo
-  echo user=root                          >> $connectinfo
-  echo password=root                      >> $connectinfo
-  echo tomcat_port=$TomcatConnectionPort  >> $connectinfo
-  echo cms_port=$CMSPort                  >> $connectinfo
+  {
+    echo BUILD_NUMBER=$version
+    echo BUILD_STREAM=$1
+    echo ip=$nodeone
+    echo user=root
+    echo password=root
+    echo tomcat_port=$TomcatConnectionPort
+    echo cms_port=$CMSPort
+  } > $connectinfo
 
   cat $connectinfo
   echo; }
 
 
+#--------------------------------------
+function SmokeTest {
+
+  # retrieve nodeone IP
+  ping=$(ping -c 1 $nodeone 2>&1 | grep "(")
+  if [ ! "${ping}" ]; then
+    echo "Failed to retrieve $nodeone IP"
+    exit 1; fi
+  IP=$(echo $ping | awk '$3 { print $3 }')
+  IP=${IP/(/}
+  IP=${IP/)/}
+
+
+  fileName="RunSmt-$1.sh"
+
+  {
+    echo "#!/bin/sh"
+    echo
+    echo "## -------------------------------------------------------------------"
+    echo "## Encapsulate the trigerring of the RM script Build.pl"
+    echo "## ------------------------------------------------------------------"
+    echo
+    echo "export RM_TOOL_HOME=/build/pblack/core.build.tools"
+    echo
+    echo "## Variables used by the perl script to generate qrs file :"
+    echo "export ARCHITECTURE=64"
+    echo "export BUILD_INI_FILE=$1.ini"
+    echo "export BUILD_VERSION=$version"
+    echo "export SMTMACHINE=$nodeone"
+    echo "export SMTMACHINE_IP=$IP"
+    echo "export TOMCATPORT=$TomcatConnectionPort"
+    echo "export CMSPORT=$CMSPort"
+    echo
+    echo "cd \$RM_TOOL_HOME"
+    echo
+    echo "#Set Build Ini File"
+    echo "RM_TOOL_INI=\$RM_TOOL_HOME/export/shared/contexts/\${BUILD_INI_FILE}"
+    echo "echo RM Ini file used : \$RM_TOOL_INI"
+    echo
+    echo "#Launch Build.pl script => launch smoke test"
+    echo "perl \$RM_TOOL_HOME/export/shared/Build.pl -\$ARCHITECTURE -dashboard -warning=0 -i=\$RM_TOOL_INI -v=\$BUILD_VERSION -S 1> \${RM_TOOL_HOME}/Buildpl_SMT.log 2>&1"
+  } > $fileName
+
+  buildMachine="dewdftvu1018.wdf.sap.corp"
+  user="pblack"
+
+  scp -oStrictHostKeyChecking=no ./$fileName $user@$buildMachine:/build/$user/tmp/$fileName
+
+  if [ $? -ne 0 ]; then
+    echo "Failed to scp '$fileName' to build machine '$buildMachine'"
+    exit 1; fi
+
+  ssh ${user}@${buildMachine} -oStrictHostKeyChecking=no /build/$user/tmp/$fileName; }
+
+
 #---------------  MAIN
 # params  aurora aurora42_cons aurora4xInstall
-
-#set -x
 
 CheckParam $#
 InitVars $1 $2 $3
 DeployContainers $4
 RetrieveDeployedNodes "$2_$version"
 WriteConnnectionFile $2
+SmokeTest $2
