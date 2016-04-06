d533980e289ec9e2769cdc9f8c5d841e09314b34
Gerald BRAUNWARTH <gerald.braunwarth@sap.com>
2016-04-06 15:25:48 +0200

    Migrate from dockerdevregistry to Artifactory

--- Redhat/build/Aurora-XI4x/jenkins/artifactoryTrigger.sh
+++ Redhat/build/Aurora-XI4x/jenkins/artifactoryTrigger.sh
@@ -5,8 +5,6 @@
 #  AUTHOR: gerald.braunwarth@sap.com    - february 2016 -
 #  PURPOSE: detects the image push of the daily drop version, in an Artifactory repository
 #
-#  WARNING: The file prevCurl.txt must exists, it contains the previous repositories list before changes
-#
 ###############################################################################
 
 set -x
@@ -17,29 +15,29 @@ set -x
 
 
 if [ $# -ne 3 ]; then
-  echo "Expected parameter <Suite> <Folder>"
+  echo "Expected parameter <Suite>  <Folder>  <xMakeRepo>"
   echo "Example: artifactoryTrigger.sh  aurora  aurora42_cons  aurora42_cons"
   exit 1; fi
 
-if [ ! -f 'prevCurl.txt' ]; then
-   curl -s -k 'https://github.wdf.sap.corp/raw/Dev-Infra-Levallois/Docker/master/Redhat/build/Aurora-XI4x/jenkins/prevCurl.txt' > prevCurl.txt
-  if [ ! -f 'prevCurl.txt' ]; then
-    echo 'Failed to get file prevCurl.txt from GitHub'
+if [ ! -f prevCurl-$2.txt ]; then
+   curl -s -k https://github.wdf.sap.corp/raw/Dev-Infra-Levallois/Docker/master/Redhat/build/Aurora-XI4x/jenkins/prevCurl/prevCurl-$2.txt > prevCurl-$2.txt
+  if [ ! -f prevCurl-$2.txt ]; then
+    echo "Failed to get file prevCurl-$2.txt from GitHub"
     exit 1; fi; fi
 
 artirepo="https://docker.wdf.sap.corp/artifactory/api/storage/cidemo/$1/"
 version=$(curl -s -k https://github.wdf.sap.corp/raw/AuroraXmake/$3/master/version.txt)
 
 if [ ! "${version}" ]; then
-  echo "Failed to retrieve version from xMake Github repo"
+  echo 'Failed to retrieve version from xMake Github repo'
   exit 1; fi
 
-curl -s -k $artirepo | grep uri > newCurl.txt
+curl -s -k $artirepo | grep uri > newCurl-$2.txt
 
-fgrep -vf prevCurl.txt newCurl.txt | grep $2_$version-snapshot
+fgrep -vf prevCurl-$2.txt newCurl-$2.txt | grep $2_$version-snapshot
 status=$?
 
-rm -f prevCurl.txt
-mv  newCurl.txt prevCurl.txt
+rm -f prevCurl-$2.txt
+mv  newCurl-$2.txt prevCurl-$2.txt
 
 exit $status
