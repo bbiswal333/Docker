c4184dd54233f34ce3aeb39a72e5695d18830476
Gerald BRAUNWARTH <gerald.braunwarth@sap.com>
2016-04-25 15:06:46 +0200

    Self download of required script

--- Redhat/build/Aurora-XI4x/jenkins/undeploy.sh
+++ Redhat/build/Aurora-XI4x/jenkins/undeploy.sh
@@ -12,14 +12,24 @@
 # clear
 
 if [ $# -ne 1 ]; then
-  echo " Expected parameter: <SuiteName>"
-  echo " Example: ./undeploy.sh  aurora"
+  echo " Expected parameter: <Name>"
+  echo " Example: ./undeploy.sh  aurora|aurora42_cons"
+  exit 1; fi
+
+
+# Download swarmHA-cmd.sh from Github
+echo
+echo "Getting 'swarmHA-cmd.sh' from Github"
+curl -s https://github.wdf.sap.corp/raw/Dev-Infra-Levallois/Docker/master/swarm/automation/swarmHA-cmd.sh > swarmHA-cmd.sh
+
+if [ ! -f swarmHA-cmd.sh ]; then
+  echo "Failed to get 'swarmHA-cmd.sh' from github"
   exit 1; fi
 
 
 # Delete containers (forcing Stop)
 echo
-echo "DELETING ALL Aurora CONTAINERS"
+echo "DELETING '$1' CONTAINERS"
 array=$(./swarmHA-cmd.sh ps -a | awk -v value=$1 '$2 ~ value {print $NF}')
 
 if [ "${array}" ]; then
@@ -28,7 +38,7 @@ if [ "${array}" ]; then
 
 # Delete images
 echo
-echo "DELETING ALL Aurora IMAGES"
+echo "DELETING '$1' IMAGES"
 array=$(./swarmHA-cmd.sh images | awk -v value=$1 '$1 ~ value {print $1}')
 
 if [ "${array}" ]; then
