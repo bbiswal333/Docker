dd2be3f48b88d3e2b0e3e2e8859f7a99cd6a640a
Gerald BRAUNWARTH <gerald.braunwarth@sap.com>
2016-04-29 16:53:14 +0200

    Operational version

--- Redhat/build/Aurora-XI4x/jenkins/artifactoryCleanup..ps1
+++ Redhat/build/Aurora-XI4x/jenkins/artifactoryCleanup..ps1
@@ -1,38 +1,38 @@
 ﻿#*************************************************************************
-# Author: gerald.braunwarth@sap.com
+# Author: gerald.braunwarth@sap.com			- April 2016 -
 # Purpose : Cleanup Artifactory keeping 3 last versions of each build type
 #*************************************************************************
 
-#  aurora  3
+# parameters: aurora  3
 
-#if ($args.Count -ne 2) {
-#  Write-Host "Expected argument: <SuiteName> <NbVersionsToKeep>
-#  Write-host "Example artifactoryCleanup.ps1  aurora  3"
-#  return 1 }
+if ($args.Count -ne 2) {
+  Write-Host "Expected argument: <SuiteName> <NbVersionsToKeep>"
+  Write-host "Example artifactoryCleanup.ps1  aurora  3"
+  exit 1 }
 
 $suite = $args[0]
 $max = $args[1]
 
 ## DEBUG
-cls
-$suite = 'aurora'
-$max = 3
+#cls
+#$suite = 'aurora'
+#$max = 3
 ## ENDDEBUG
 
 $AllBuild = 'aurora_pi_aolap', 'aurora_pi_po', 'aurora_pi_tp', 'aurora_rel_cs', 'aurora42_cons', 'aurora42_cons_ml'
 
+$registry = 'https://docker.wdf.sap.corp'
+$ports    = '51003', '51020'
+$repos    = 'cidemo', 'xmake_snapshot'
+
 $apiKey = 'AKCp2UNNGgbwi9YrxsAXiGdtMN8FLaTumzzMNiXs2xELzfEDGp9NnqsHhQPK9EXJM8vTsHDC9'
 $header = @{"X-Jfrog-Art-Api" = $apiKey}
 
-$registry		= 'https://docker.wdf.sap.corp'
-$virtualdocker	= "$registry/artifactory/virtual_docker/$suite"
-$reposURL		= '51003/artifactory/cidemo', '51020/artifactory/xmake_snapshot'
-
-$html = Invoke-WebRequest -Uri $virtualdocker
+$html = Invoke-WebRequest -Uri "$registry/artifactory/virtual_docker/$suite"
 
 if (-not $html.AllElements.Count) {
   Write-Host 'Failed to retrieve Aurora images list from Artifactory server'
-  return 1 }
+  exit 1 }
 
 $href = $html.AllElements | select href | where { $_ -match $suite } | sort -Property href
 
@@ -42,10 +42,10 @@ foreach ($build in $AllBuild) {
   $NbDelete = $versions.Count - $max
   
   for ($i = 0; $i -lt $NbDelete; $i++) {
-	$result = Invoke-RestMethod -ErrorVariable Err -ErrorAction SilentlyContinue -Method Delete -Header $header -Uri "$($registry):$($reposURL[0])/$suite/$($versions[0].href)"
-	$result = Invoke-RestMethod -ErrorVariable Err -ErrorAction SilentlyContinue -Method Delete -Header $header -Uri "$($registry):$($reposURL[1])/$suite/$($versions[0].href)"
+    for ($j = 0; $j -lt 2; $j++) {
+	  $result = Invoke-RestMethod -ErrorVariable Err -ErrorAction SilentlyContinue -Method Delete -Header $header -Uri "$($registry):$($ports[$j])/artifactory/$($repos[$j])/$suite/$($versions[0].href)" }
     $versions.RemoveAt(0) }}
 
 # Empty Recycle Bin
-$result = Invoke-RestMethod -ErrorVariable Err -ErrorAction SilentlyContinue -Method Delete -Header $header -Uri "$($registry)/api/trash/cidemo/$suite"
-$result = Invoke-RestMethod -ErrorVariable Err -ErrorAction SilentlyContinue -Method Delete -Header $header -Uri "$($registry)/api/trash/xmake_snapshot/$suite"
+foreach ($repo in $repos) {
+  $result = Invoke-RestMethod -ErrorVariable Err -ErrorAction SilentlyContinue -Method Delete -Header $header -Uri "$($registry)/artifactory/api/trash/clean/$repo/$suite" }
