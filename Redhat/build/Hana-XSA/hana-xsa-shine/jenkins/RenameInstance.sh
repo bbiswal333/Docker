a11bc9694c6050efda9dc2af7896b48a2b3b1dc8
Gerald BRAUNWARTH <gerald.braunwarth@sap.com>
2016-08-09 16:23:33 +0200

    Deploys Shine container

--- Redhat/build/Hana-XSA/hana-xsa-shine/jenkins/RenameInstance.sh
+++ Redhat/build/Hana-XSA/hana-xsa-shine/jenkins/RenameInstance.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 
+###############################################################################
+#
+#  AUTHOR: gerald.braunwarth@sap.com    - August 2016 -
+#  PURPOSE: rename the Hana instance at the container startup
+#
+###############################################################################
+
 set -x
+
+if [ $# -ne 2 ]; then
+  echo "Expected parameters: <SID>  <InstanceNumber>"
+  echo "Example: ./RenameInstance.sh  DCK  00"
+  exit 1; fi
+
+newsid=$1
+newnumber=$2
 
 OLDHOST=$($saphome/shared/$sid/hdblcm/hdblcm --list_systems | grep -i 'host:' | awk '$2 { print tolower($2) }')
 NEWHOST=$(echo $(hostname -f) | awk '{ print tolower($0) }')
-newsid='SI5'
-newnumber='05'
 
-#$saphome/shared/$sid/hdblcm/hdblcm -b --action=rename_system --source_password=$secret --target_password=$secret --hostmap=$OLDHOST=$NEWHOST
-$saphome/shared/$sid/hdblcm/hdblcm -b --action=rename_system \
-                                      --source_password=$secret --target_password=$secret \
-                                      --hostmap=$OLDHOST=$NEWHOST \
-                                      --source_sid=$sid --target_sid=$newsid \
-                                      --number=$newnumber
-echo $?
+
+#$saphome/shared/$sid/hdblcm/hdblcm -b --action=rename_system \
+#                                      --source_password=$secret --target_password=$secret \
+#                                      --hostmap=$OLDHOST=$NEWHOST
+
+if ! $saphome/shared/$sid/hdblcm/hdblcm -b --action=rename_system \
+                                           --source_password=$secret --target_password=$secret \
+                                           --hostmap=$OLDHOST=$NEWHOST \
+                                           --source_sid=$sid --target_sid=$newsid \
+                                           --number=$newnumber; then
+  echo "Failed to rename instance";
+  exit 1; fi
---
+++ Redhat/build/Hana-XSA/hana-xsa-shine/jenkins/deploy-shine.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+###############################################################################
+#
+#  AUTHOR: gerald.braunwarth@sap.com    - August 2016 -
+#  PURPOSE: deploy a hana-xsa-shine container from the image
+#
+###############################################################################
+
+
+#--------------------------------------
+function OnError {
+  echo
+  echo $1
+  echo
+  exit 1; }
+
+
+#--------------------------------------
+function DeleteContainers {
+  dummy=$(docker ps -a -q)
+  if [ "${dummy}" ]; then
+    docker rm -f -v $dummy; fi; }
+
+
+#--------------------------------------
+function DeleteImages {
+  dummy=$(docker images -q)
+  if [ "${dummy}" ]; then
+    docker rmi $dummy; fi; }
+
+
+
+#---------------  MAIN
+set -x
+
+if [ $# -ne 2 ]; then
+  echo "Expected parameters: <SID>  <InstanceNumber>"
+  echo "Example: ./RenameInstance.sh  DCK  00"
+  exit 1; fi
+
+image="hana-xsa-shine"
+imgPull=docker.wdf.sap.corp:50000/hanaxsshine/weekstone/$image
+curlScript="RenameInstance.sh"
+
+echo "Cleanup host, delete existing containers and images"
+DeleteContainers
+DeleteImages
+
+echo "Getting '$curlScript' from github"
+if ! curl -s -k https://github.wdf.sap.corp/raw/Dev-Infra-Levallois/Docker/master/Redhat/build/Hana-XSA/hana-xsa-shine/jenkins/$curlScript > $curlScript; then
+  OnError "Failed to curl '$curlScript' from Github"; fi
+
+echo "Deploying container"
+pth=$(pwd)
+if ! docker run -t --net=host -v $pth:/scripts $imgPull "/scripts/$curlScript $1 $2"; then
+  OnError "Failed to start '$image' container"; fi
