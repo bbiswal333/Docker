98923c4a2acde2df6d9b804736f61640146dcd28
Gerald BRAUNWARTH <gerald.braunwarth@sap.com>
2016-09-30 13:24:01 +0200

    backup of on-going deve

--- Redhat/build/Hana-XSA/hana-xsa/jenkins/NexusTrigger-xsa.ps1
+++ Redhat/build/Hana-XSA/hana-xsa/jenkins/NexusTrigger-xsa.ps1
@@ -13,69 +13,109 @@
 
 
 # --------------------------------
-FUNCTION historize($new, $old) {
-  Move-Item -Force $new diff\$old }
+FUNCTION historize($name) {
+  Move-Item -Force $name diff\$name }
 
 
 # --------------------------------
-FUNCTION OnNewVersion($folder, $fileName) {
-  $folders[$folders.Count-1] -replace '/','' | Out-File $fileName }
+#FUNCTION BuildMask ($masks) {
+#  $mask = ""
+#  foreach ($item in $masks) {
+#    $mask += "|$item" }
+#  $mask = $mask.Remove(0,1)
+#  return $mask }
 
+FUNCTION CreateUploadList($zone, $release) {
+  $list = Invoke-WebRequest "$rootUrl/$($zone.url)/$release"
+  $files = $list.Links | foreach { $_.outerText } | where { $_ -notmatch 'parent' }
+#  $mask = BuildMask $zone.mask
+  $limit = if ($zone.mask.Count -gt 1) {$zone.mask.Count-1} else {0}
+  $installers = $files | where { $_ -like "$($zone.mask[0])" -or $_ -like "$($zone.mask[$limit])" }
+  
+  foreach ($file in $installers) {
+    "$rootUrl/$($zone.url)/$file" | Out-File "upload-$($zone.name).txt" -Append }}
+
+# Get-EventLog Security | ?{$_.Username -notmatch '^user1$|^.*user$'}
 
 #********************************************************************************************
 #      MAIN
 #********************************************************************************************
 
 $rootUrl = "http://nexus.wdf.sap.corp:8081/nexus/content/repositories"
-$prefix  = "dropZone"
 
-$dropzones = [pscustomobject] @{name = "xsrt";     url = "deploy.releases.xmake/com/sap/xs/onpremise/runtime/xs.onpremise.runtime.hanainstallation_linuxx86_64"},
-             [pscustomobject] @{name = "admin";    url = "deploy.milestones.xmake/com/sap/xsa/admin/sap-xsac-admin"},
-             [pscustomobject] @{name = "schedule"; url = "deploy.milestones.xmake/com/sap/xs/jobscheduler/jobscheduler-assembly"},
-             [pscustomobject] @{name = "shine";    url = "deploy.milestones.xmake/com/sap/refapps/sap-xsac-shine"},
-             [pscustomobject] @{name = "hrtt";     url = "deploy.milestones/com/sap/xsa/hrtt/sap-xsac-hrtt"},
-             [pscustomobject] @{name = "di";       url = "deploy.milestones/com/sap/di/sap-xsac-di"},
-             [pscustomobject] @{name = "webide";   url = "deploy.milestones/com/sap/devx/sap-xsac-webide"}
+$dropzones = [pscustomobject] @{name = "xsrt";     upload="RT";   mask = @("xs.onpremise.runtime.hanainstallation*[0-9].SAR"); url = "deploy.releases.xmake/com/sap/xs/onpremise/runtime/xs.onpremise.runtime.hanainstallation_linuxx86_64"},
+             [pscustomobject] @{name = "admin";    upload="XSA";  mask = @("*MONITORING*[0-9].zip");                           url = "deploy.milestones.xmake/com/sap/xsa/admin/sap-xsac-admin"},
+             [pscustomobject] @{name = "schedule"; upload="root"; mask = @("jobscheduler-assembly*[0-9].zip");                 url = "deploy.milestones.xmake/com/sap/xs/jobscheduler/jobscheduler-assembly"},
+#            [pscustomobject] @{name = "shine";    upload="XSA";  mask = @("XSACSHINE*[0-9].zip");                             url = "deploy.milestones.xmake/com/sap/refapps/sap-xsac-shine"},
+             [pscustomobject] @{name = "hrtt";     upload="XSA";  mask = @("sap-xsac-hrtt*[0-9].zip");                         url = "deploy.milestones/com/sap/xsa/hrtt/sap-xsac-hrtt"},
+             [pscustomobject] @{name = "di";       upload="XSA";  mask = @("*XSACDICORE*[0-9].zip","*[0-9].mtaext");           url = "deploy.milestones/com/sap/di/sap-xsac-di"},
+             [pscustomobject] @{name = "webide";   upload="XSA";  mask = @("*XSACSAPWEBIDE[0-9]*.zip","*[0-9].mtaext");        url = "deploy.milestones/com/sap/devx/sap-xsac-webide"}
 cls
 Set-Location (Split-Path $MyInvocation.MyCommand.Path)
 
-Remove-Item "newVersion-*.txt" -ErrorAction SilentlyContinue
-
 if (-not (Test-Path diff)) {
   mkdir diff }
-
+ 
 foreach ($zone in $dropzones) {
 
-	$name = "$prefix-$($zone.name)"
-	$OLD = "$name-OLD.txt"
-	$NEW = "$name-NEW.txt"
-	$newVersion = "newVersion-$($zone.name).txt"
+	$releaseTxt = "release-$($zone.name).txt"
 
-	$html = Invoke-WebRequest "$rootUrl/$($zone.url)"
-	$folders = $html.AllElements | where { $_.tagName -EQ "A" -and $_.outerText -match "/" } | select outerText | foreach { $_.outerText } | sort
+	Invoke-WebRequest "$rootUrl/$($zone.url)/maven-metadata.xml" -OutFile $releaseTxt
 	
-	$folders | Out-File $NEW
+	if (-not (Test-Path $releaseTxt)) {
+	  Write-Host "Failed to download '$rootUrl/$($zone.url)/maven-metadata.xml'"
+	  exit 1 }
 
-    # First exec : consider last drop as a new drop
-	if (-not (Test-Path diff\$OLD)) {
-	  OnNewVersion $folders $newVersion
-	  historize $NEW $OLD
-	  continue }
-
-	$diff = @(Compare-Object (get-Content diff\$OLD) (get-Content $NEW))
-	$diff = $diff | where { $_.SideIndicator -eq '=>' }
-
-	if ($diff.Count) {
-	  if ($diff.Count -gt 1) {
-	    $diff = $diff | sort -Property InputObject }
-	  OnNewVersion $folders $newVersion }
+	$content = Get-Content $releaseTxt
+	$release = ($content | where { $_ -match 'release' }) -replace '</release>','' -replace '<release>','' -replace ' ',''
 	
-    historize $NEW $OLD }
+	$release | Out-File $releaseTxt
+
+	if ((Test-Path diff\$releaseTxt)) {		# First exec : consider last drop as a new drop
+      if ((Get-Content $releaseTxt) -eq (Get-Content diff\$releaseTxt)) {
+        continue }}
+
+    CreateUploadList $zone $release
+    historize $releaseTxt }
+
+
+# --------------------------------
+#FUNCTION OnNewVersion($url, $folders) {
+#
+#  $version = $folders[$folders.Count-1]
+#  $FullUrl = "$rootUrl/$url/$version"
+#
+#  $html = Invoke-WebRequest "$FullUrl"
+#  $files = $html | foreach { $_.Links.OuterText } | where {$_ -ne 'Parent Directory'} }
+
+
+#foreach ($zone in $dropzones) {
+#
+#	$name = "$prefix-$($zone.name)"
+#	$OLD = "$name-OLD.txt"
+#	$NEW = "$name-NEW.txt"
+#	$newVersion = "newVersion-$($zone.name).txt"
+#
+#	$html = Invoke-WebRequest "$rootUrl/$($zone.url)/
+#	$folders = $html.AllElements | where { $_.tagName -EQ "A" -and $_.outerText -match "/" } | select outerText | foreach { $_.outerText } | sort
+#	
+#	$folders | Out-File $NEW
+#
+#    # First exec : consider last drop as a new drop
+#	if (-not (Test-Path diff\$OLD)) {
+#	  OnNewVersion $folders $newVersion
+#	  historize $NEW $OLD
+#	  continue }
+#
+#	$diff = @(Compare-Object (get-Content diff\$OLD) (get-Content $NEW))
+#	$diff = @($diff | where { $_.SideIndicator -eq '=>' })
+#
+#	if ($diff.Count) {
+#	  if ($diff.Count -gt 1) {
+#	    $diff = $diff | sort -Property InputObject }
+#	  OnNewVersion $zone.url $folders }
+#	
+#    historize $NEW $OLD }
 
 
 #	$folders = $html.ParsedHtml.body.getElementsByTagName("TR") | select outerText | where {$_.outerText -match "/" } | foreach { $_.outerText }
-
-    # Component dropZone is empty !
-#	if (-not $folders.Count) {
-#	  historize $NEW $OLD
-#	  continue 	}
